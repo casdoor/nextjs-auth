977b0500b79d3403dc9187b60061d326ea5eb1ff
ysf <1807100869@qq.com>
2024-01-21 19:48:33 +0800

    docs: add readme、License

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,3 +1,16 @@
+// Copyright 2024 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 "use client";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
@@ -15,38 +28,40 @@ export default function Page() {
     <>
       <h1>Hello {userJson.name}!</h1>
       <hr />
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>user name</td>
-            <td>{userJson.name}</td>
-          </tr>
-          <tr>
-            <td>user avatar</td>
-            <td>{userJson.avatar}</td>
-          </tr>
-          <tr>
-            <td>user id</td>
-            <td>{userJson.sub}</td>
-          </tr>
-          <tr>
-            <td>user owner</td>
-            <td>{userJson.owner}</td>
-          </tr>
-          <tr>
-            <td>user created time</td>
-            <td>{userJson.createdTime}</td>
-          </tr>
-        </tbody>
-      </table>
-      <br />
-      <button onClick={handleLogout}>logout</button>
+      <div className="container">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>user name</td>
+              <td>{userJson.name}</td>
+            </tr>
+            <tr>
+              <td>user avatar</td>
+              <td>{userJson.avatar}</td>
+            </tr>
+            <tr>
+              <td>user id</td>
+              <td>{userJson.sub}</td>
+            </tr>
+            <tr>
+              <td>user owner</td>
+              <td>{userJson.owner}</td>
+            </tr>
+            <tr>
+              <td>user created time</td>
+              <td>{userJson.createdTime}</td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <button onClick={handleLogout}>logout</button>
+      </div>
     </>
   );
 }
