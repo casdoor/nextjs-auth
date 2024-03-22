f08e565c5bd88d7a6e4601ece4ba7daf8b29bee9
SamYSF <98792230+SamYSF@users.noreply.github.com>
2024-03-23 01:18:52 +0800

    feat: remove client secret (#8)

--- app/conf.js
+++ app/conf.js
@@ -1,7 +1,6 @@
 const sdkConfig = {
   serverUrl: "https://door.casdoor.com",
   clientId: "294b09fbc17f95daf2fe",
-  clientSecret: "dd8982f7046ccba1bbd7851d5c1ece4e52bf039d",
   organizationName: "casbin",
   appName: "app-vue-python-example",
   redirectPath: "/callback",
